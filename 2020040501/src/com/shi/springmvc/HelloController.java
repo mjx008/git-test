dd68bbb6bd378458e139fc5ed1e2632da36b0fb3
maojx <1299490476@qq.com>
2020-04-05 11:29:27 +0800

    初始化导入

---
+++ 2020040501/src/com/shi/springmvc/HelloController.java
@@ -0,0 +1,19 @@
+package com.shi.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping(value = "/sayHi",method = {RequestMethod.POST,RequestMethod.GET})
+    @ResponseBody
+    public String hello(@RequestParam("heello") String hello){
+        System.out.println("进入该方法123");
+        return "success";
+    }
+}
---
+++ 2020040501/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/4/5 0005
+  Time: 9:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$1222222222334</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
